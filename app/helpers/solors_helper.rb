2cc9d0f6f26e7ea5ea00b08b8f574af1880af481
tech-kazuki <uekazuki0411@akiyamakazukinoMacBook-puro.local>
2019-04-10 10:57:41 +0900

    Initialize solar

---
+++ app/helpers/solors_helper.rb
@@ -0,0 +1,2 @@
+module SolorsHelper
+end
---
+++ test/controllers/solors_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class SolorsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
